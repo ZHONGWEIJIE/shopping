0dc89668f5e10e5671a9c858bca16ebbcc6cecb1
Mr.钟 <997041772@qq.com>
2019-06-23 11:41:08 +0800

    修改Controller

--- src/main/java/com/zhong/controller/GuestBookController.java
+++ src/main/java/com/zhong/controller/GuestBookController.java
@@ -1,8 +1,11 @@
 package com.zhong.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.zhong.entity.GuestBook;
 import com.zhong.service.GuestBookService;
+import com.zhong.utils.EmailUtils;
 import com.zhong.utils.Result;
+import com.zhong.utils.StatysInfo;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -18,6 +21,13 @@ public class GuestBookController {
     @Resource
     GuestBookService guestBookService;
 
+
+    @RequestMapping("/sendEmail")
+    public String sendEmail() throws JsonProcessingException {
+        boolean isSend = EmailUtils.sendEmail("这是一封测试邮件", new String[]{"997041772@qq.com"}, null, "<h3><a href='http://www.baidu.com'>百度一下，你就知道</a></h3>", null);
+        return "发送邮件:" + isSend;
+    }
+
     @GetMapping({"/getAll/{adminId}" })
     public Result selectGuestBook(@PathVariable String adminId){
         if (guestBookService.isAdmin(adminId)) {
@@ -25,7 +35,7 @@ public class GuestBookController {
             return Result.makeSuccessResult(map);
         }
         else {
-            return Result.makePermissionDeniedResult();
+            return Result.makeResult("400",new StatysInfo("没有权限","对不起，您没有权限"));
         }
     }
 
@@ -40,7 +50,7 @@ public class GuestBookController {
             return Result.makeSuccessResult(map);
         }else {
             map.put("guestBookList",new ArrayList<GuestBook>());
-            return Result.makeResult("404", "404 Not Found",map);
+            return Result.makeResult("404",new StatysInfo("404 Not Found"),map);
         }
     }
 
@@ -48,9 +58,9 @@ public class GuestBookController {
     public Result updateReplyById(String id,String content){
         Map<String, Object> map = new HashMap<>();
         if (guestBookService.updateReplyById(id,content)){
-            return Result.makeSuccessResult("回复成功");
+            return Result.makeSuccessResult(new StatysInfo("Success","回复成功"));
         }else {
-            return  Result.makeFailResult("回复失败");
+            return  Result.makeResult("10002",new StatysInfo("Fail","回复失败"));
         }
 
     }
